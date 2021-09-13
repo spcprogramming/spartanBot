dab03e8365c39de66d93fe3be4a689c0570ac1d7
RustyRin <ggk494@gmail.com>
2021-09-13 13:13:57 -0400

    tall

--- commands/SPCEvents/scan.py
+++ commands/SPCEvents/scan.py
@@ -208,7 +208,7 @@ def main(url="https://www.spcollege.edu/events"):
     #chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     #chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--window-size=1920,3000")
     chrome_options.add_argument('--ignore-certificate-errors')
     chrome_options.add_argument('--allow-running-insecure-content')
     chrome_options.add_argument('--disable-dev-shm-usage')

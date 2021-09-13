e827c505b32838d75575d4510ccfc9aff33963f0
RustyRin <ggk494@gmail.com>
2021-09-13 12:47:46 -0400

    args

--- commands/SPCEvents/scan.py
+++ commands/SPCEvents/scan.py
@@ -205,9 +205,9 @@ def main(url="https://www.spcollege.edu/events"):
     chrome_options.add_argument("--remote-debugging-port=9222")
     #chrome_options.add_argument("--disable-extensions")
     #chrome_options.add_argument("--disable-gpu")
-    '''chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--window-size=2560,1440")
+    chrome_options.add_argument("--no-sandbox")
+    #chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument('--ignore-certificate-errors')
     chrome_options.add_argument('--allow-running-insecure-content')
     chrome_options.add_argument('--disable-dev-shm-usage')
@@ -215,7 +215,7 @@ def main(url="https://www.spcollege.edu/events"):
     chrome_options.add_argument("--start-maximized")
     chrome_options.add_argument("--proxy-bypass-list=*")
     chrome_options.add_argument("--proxy-server='direct://'")
-    chrome_options.add_argument("--disable-extensions")'''
+    chrome_options.add_argument("--disable-extensions")
     # chrome_options.headless = True # also works
     driver = webdriver.Firefox(options=chrome_options)
     driver.get(url)

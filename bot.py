bede735e212edabc7b16f62f47992fc5ccf9e1c0
RustyRin <ggk494@gmail.com>
2021-09-13 11:58:58 -0400

    new time

--- bot.py
+++ bot.py
@@ -148,7 +148,7 @@ async def checkTime():
 	'''
 	scanning for new ones
 	'''
-	scanTime = '11:51'
+	scanTime = '12:02'
 	timeformat = '%H:%M'
 	timeNow = datetime.strftime(datetime.now(), timeformat)
 

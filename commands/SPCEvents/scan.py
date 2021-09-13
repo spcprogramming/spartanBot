13a34ccf713f84eda5c99ac6f913d694d4203766
RustyRin <ggk494@gmail.com>
2021-09-13 11:17:08 -0400

    remove dev-shm-uasage chrome arg

--- commands/SPCEvents/scan.py
+++ commands/SPCEvents/scan.py
@@ -204,7 +204,7 @@ def main(url="https://www.spcollege.edu/events"):
     #chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox") # linux only
     chrome_options.add_argument("--headless")
-    chrome_options.add_argument('--disable-dev-shm-usage')
+    #chrome_options.add_argument('--disable-dev-shm-usage')
     # chrome_options.headless = True # also works
     driver = webdriver.Chrome(options=chrome_options)
     driver.get(url)

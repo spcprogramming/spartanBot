3ff5f411b9d511671b8f328c992d10fb0a06f5b2
RustyRin <ggk494@gmail.com>
2021-09-13 11:46:31 -0400

    even more args

--- commands/SPCEvents/scan.py
+++ commands/SPCEvents/scan.py
@@ -204,10 +204,15 @@ def main(url="https://www.spcollege.edu/events"):
     #chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox") # linux only
     chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--window-size=2560,1440")
     chrome_options.add_argument('--ignore-certificate-errors')
     chrome_options.add_argument('--allow-running-insecure-content')
-    #chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--proxy-bypass-list=*")
+    chrome_options.add_argument("--proxy-server='direct://'")
+    chrome_options.add_argument("--disable-extensions")
     # chrome_options.headless = True # also works
     driver = webdriver.Chrome(options=chrome_options)
     driver.get(url)

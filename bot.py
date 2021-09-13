981c598243f61e01e5e5c4c5465230ab9d4199be
RustyRin <ggk494@gmail.com>
2021-09-13 11:17:29 -0400

    change time to test

--- bot.py
+++ bot.py
@@ -148,7 +148,7 @@ async def checkTime():
 	'''
 	scanning for new ones
 	'''
-	scanTime = '11:10'
+	scanTime = '11:20'
 	timeformat = '%H:%M'
 	timeNow = datetime.strftime(datetime.now(), timeformat)
 
